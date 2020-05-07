765cbbc820ab6a32c4715f044b1f131ce545712e
Kittikawin <kawin356@gmail.com>
2020-05-07 15:20:56 +0700

    Initial Commit

---
+++ WordScramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Kittikawin Sontinarakul on 7/5/2563 BE.
+//  Copyright © 2563 Kittikawin Sontinarakul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
